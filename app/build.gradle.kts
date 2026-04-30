17774b536bc157bcbf480267b93205358dfa09dd
Malte Kiefer <malte.kiefer@mailbox.org>
2026-04-30 09:27:20 +0200

    chore(release): bump version to 1.0.0 (versionCode 100)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "de.kiefer_networks.falco"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 100
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
